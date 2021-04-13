cba516d41ba00262215c2c7bb29a803956df3a06
Maxime Mouchet <max@maxmouchet.com>
2021-04-13 14:50:22 +0200

    Update ftp-sync.sh

--- ftp-sync.sh
+++ ftp-sync.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 src=exports/
-dst=iris@venus.planet-lab.eu:/srv/icg-ftp/snapshots_2021
+dst=venus.planet-lab.eu:/srv/icg-ftp/snapshots_2021
 
 find "${src}" -name '*.clickhouse' -exec lz4 -f --rm "{}" \;
 rsync --archive --delete --progress "${src}" "${dst}"
