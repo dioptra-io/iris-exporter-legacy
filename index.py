7bb3cf652a89cc95bf5d29f603e016f7fc110919
Maxime Mouchet <max@maxmouchet.com>
2021-04-27 11:21:39 +0200

    index: add nodes/links counts

--- index.py
+++ index.py
@@ -1,24 +1,46 @@
 import json
 from datetime import datetime
 from pathlib import Path
+from subprocess import run
+
+
+def start_time(measurement):
+    return datetime.fromisoformat(measurement["start_time"])
+
+
+def end_time(measurement):
+    return datetime.fromisoformat(measurement["end_time"])
+
+
+def wc(file):
+    res = run(["wc", "-l", file], capture_output=True)
+    return int(res.stdout.split()[0])
 
 
 def main():
     files = Path("exports/").glob("*.json")
-    metas = [json.loads(f.read_text()) for f in files]
-    metas = sorted(metas, key=lambda x: datetime.fromisoformat(x["start_time"]))
+    metas = []
+    for file in files:
+        meta = json.loads(file.read_text())
+        nodes = wc(file.with_suffix(".nodes"))
+        links = wc(file.with_suffix(".links"))
+        metas.append((meta, nodes, links))
+    metas = sorted(metas, key=lambda x: datetime.fromisoformat(x[0]["start_time"]))
 
     md = ""
-    template = "{:10} | {:15} | {:8} | {:20} | {:20}"
-    md += template.format("uuid", "tool", "agents", "start", "end") + "\n"
-    md += template.format("--", "--", "--", "--", "--") + "\n"
-    for m in metas:
+    template = "{:<10} | {:<20} | {:<20} | {:<14} | {:<10} | {:<10}"
+    md += (
+        template.format("UUID", "Start", "End", "Duration (h)", "Nodes", "Links") + "\n"
+    )
+    md += template.format("--", "--", "--", "--", "--", "--") + "\n"
+    for meta, nodes, links in metas:
         md += template.format(
-            m["uuid"].split("-")[0],
-            m["tool"],
-            len(m["agents"]),
-            m["start_time"],
-            m["end_time"],
+            meta["uuid"].split("-")[0],
+            meta["start_time"],
+            meta["end_time"],
+            (end_time(meta) - start_time(meta)).seconds // 3600,
+            nodes,
+            links,
         )
         md += "\n"
 
