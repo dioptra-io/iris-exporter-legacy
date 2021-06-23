037cca03a2d8cba809b3b1ee2809a7c8177d598a
Maxime Mouchet <max@maxmouchet.com>
2021-06-23 17:07:26 +0200

    Update iris-cron.sh

--- iris-cron.sh
+++ iris-cron.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 docker run \
+  --rm \
   --env IRIS_USERNAME=admin \
   --env IRIS_PASSWORD=randompassword \
   --network iris_default \
@@ -9,10 +10,12 @@ docker run \
   iris-exporter export --host clickhouse --database iris --destination /exports --tag mindef.saturday.json
 
 docker run \
+  --rm \
   --volume /srv/clones/iris-exporter/exports:/exports \
   iris-exporter index --destination /exports
 
 docker run \
+  --rm \
   --volume /home/dioptra-bot/.ssh:/root/.ssh \
   --volume /srv/clones/iris-exporter/exports:/exports \
   iris-exporter sync --source /exports --destination dioptra-bot@venus.planet-lab.eu:/srv/icg-ftp/snapshots_2021
