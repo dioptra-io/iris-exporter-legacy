7cef0fb9eb6438856d10e5fb8e55d5fcf629aa9c
Elena Nardi <76261381+Elenetta17@users.noreply.github.com>
2026-04-29 09:55:06 +0200

    Fix/strip schema ttl settings (#45)

--- iris_exporter.py
+++ iris_exporter.py
@@ -191,6 +191,8 @@ async def do_export_schema(
     """
     if not file.exists():
         await clickhouse(host, database, user, password, query)
+        lines = file.read_text().splitlines()
+        file.write_text("\n".join(lines[:-2]) + "\n")
 
 
 async def do_export_table(
@@ -241,6 +243,8 @@ async def do_export_links_schema(
     """
     if not file.exists():
         await clickhouse(host, database, user, password, query)
+        lines = file.read_text().splitlines()
+        file.write_text("\n".join(lines[:-2]) + "\n")
 
 
 async def do_export_links_table(
